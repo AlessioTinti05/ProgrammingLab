d77ddd650ec1eb04a28c7ea456cfea76c02106cd
AlessioTinti05 <alessiotinti05@gmail.com>
2024-12-04 10:36:36 +0100

    Scelta per input

--- EX-3_foglio6.py
+++ EX-3_foglio6.py
@@ -1,15 +1,18 @@
 def scelta (input):
-    input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-    for input in {"1", "2", "3"}:
-        if input == 1:
-            valore1 = input("Scegli il primo numero: \n")
-            valore2 = input ("Scegli il secondo valore: \n")
-            print (valore1 + valore2)
-            input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-        elif input == 2:
-            valore1 = input("Scegli il primo numero: \n")
-            valore2 = input ("Scegli il secondo valore: \n")
-            print (valore1 - valore2)
-            input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-        elif input == 3:
-            return ("Grazie per aver giocato")
+    while true:
+        input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
+        for input in {"1", "2", "3"}:
+            if input == "1":
+                valore1 = input("Scegli il primo numero: \n")
+                valore2 = input ("Scegli il secondo valore: \n")
+                print (valore1 + valore2)
+                input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
+            elif input == "2":
+                valore1 = input("Scegli il primo numero: \n")
+                valore2 = input ("Scegli il secondo valore: \n")
+                print (valore1 - valore2)
+                input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
+            elif input == "3":
+                return ("Grazie per aver giocato")
+            else:
+                raise Exception ("Scelta non valida. hai inserito {}, devi inserire 1, 2 o 3".format(input))
