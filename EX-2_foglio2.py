af567865131ad7c473736536ea7fd69332d25bf7
AlessioTinti05 <alessiotinti05@gmail.com>
2024-12-04 10:23:22 +0100

    Dizionario contaparole in file

--- EX-2_foglio2.py
+++ EX-2_foglio2.py
@@ -1,9 +1,9 @@
 def ContaParole (doc):
     with open (doc, 'r') as file:
-    diz = {}
-    contenuto = file.read()
-    words = contenuto.split()
-    for word in words:
-        count = contenuto.count(word)
-        diz ['word'] = count
+        diz = {}
+        contenuto = file.read()
+        words = contenuto.split()
+        for word in words:
+            count = contenuto.count(word)
+            diz ['word'] = count
     print(diz)
