6f9a0c3ffb9559668700e78dba28ffa9cd1875dd
AlessioTinti05 <alessiotinti05@gmail.com>
2024-12-04 10:17:57 +0100

    quadrato se int

--- EX-2_foglio6.py
+++ EX-2_foglio6.py
@@ -1,7 +1,7 @@
 def quadrato (valore):
     valore = input("Inserisci un valore \n")
     if type(valore) != int:
-        raise Exception("Input non valido, devi inserire un numero intero, hai inseiro il tipo {}".format(type(valore)))
+        raise Exception("Input non valido, devi inserire un numero intero, hai inserito il tipo {}".format(type(valore)))
     else:
         valore = valore*valore
         return valore
