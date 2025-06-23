8a3a5e228bbe7721965e6d6d83c8715e9d3bc661
AlessioTinti05 <alessiotinti05@gmail.com>
2025-06-23 18:09:44 +0200

    miglioramento

--- EX-3_foglio6.py
+++ EX-3_foglio6.py
@@ -1,18 +1,16 @@
-def scelta (input):
-    while input in ["1", "2", "3"]:
-        input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-        for input in {"1", "2", "3"}:
-            if input == "1":
-                valore1 = input("Scegli il primo numero: \n")
-                valore2 = input ("Scegli il secondo valore: \n")
-                print (valore1 + valore2)
-                input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-            elif input == "2":
-                valore1 = input("Scegli il primo numero: \n")
-                valore2 = input ("Scegli il secondo valore: \n")
-                print (valore1 - valore2)
-                input = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire")
-            elif input == "3":
-                return ("Grazie per aver giocato")
-            else:
-                raise Exception ("Scelta non valida. hai inserito {}, devi inserire 1, 2 o 3".format(input))
+def scelta():
+    while True:
+        scelta = input("Scegli: 1 per sommare, 2 per sottrarre, 3 per uscire\n")
+        if scelta == "1":
+            valore1 = float(input("Scegli il primo numero: \n"))
+            valore2 = float(input("Scegli il secondo valore: \n"))
+            print("Risultato:", valore1 + valore2)
+        elif scelta == "2":
+            valore1 = float(input("Scegli il primo numero: \n"))
+            valore2 = float(input("Scegli il secondo valore: \n"))
+            print("Risultato:", valore1 - valore2)
+        elif scelta == "3":
+            print("Grazie per aver giocato")
+            break
+        else:
+            print(f"Scelta non valida. Hai inserito {scelta}, devi inserire 1, 2 o 3.")
